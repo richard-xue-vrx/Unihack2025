e52bf56c18d8c39609eb2b622ba609b5611cb3b7
Damon Lee <damonlee0497@gmail.com>
2025-03-16 13:57:48 +1100

    dwajhdw

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center h-screen gap-8 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 p-8 caret-transparent">
       <main className="flex flex-col space-y-4 min-w-[480px] max-w-[600px] p-6 bg-white rounded-lg shadow-lg">
         <div className="text-2xl text-center font-semibold">🏆 Love Leaderboard 🏆</div>
-        
+
         <div className="text-sm text-center text-gray-500">
           {leaderboard.length === 0 ? (
             <div className="text-center text-gray-500">Loading...</div>
